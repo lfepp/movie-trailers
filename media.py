d305e0748565368cd74a6fa7f63292272c9d2c2e
Luke Epp <lucasfepp@gmail.com>
2016-07-13 08:44:37 -0700

    Initial commit

---
+++ media.py
@@ -0,0 +1,7 @@
+# Define Movie class
+class Movie():
+    def __init__(self, title, year, poster_image_url, trailer_youtube_url):
+        self.title = title
+        self.year = year
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
